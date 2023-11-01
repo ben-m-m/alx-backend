13455ec742f3e59e30616761144b5753974fa4fb
ben-m-m <mainaben23@gmail.com>
2023-11-01 14:29:45 +0300

    4 correct

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,6 +20,7 @@ class Config:
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
+
 @babel.localeselector
 def get_locale():
     """
@@ -30,7 +31,6 @@ def get_locale():
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 
 @app.route('/', strict_slashes=False)
