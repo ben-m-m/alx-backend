7721fff748f154ff59678768383043792ac86714
ben-m-m <mainaben23@gmail.com>
2023-11-01 11:05:04 +0300

    parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """basic flask app
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
