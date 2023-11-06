161ffa5adc384013a42ee3be266e668d3bff9b02
ben-m-m <mainaben23@gmail.com>
2023-11-06 10:35:01 +0300

    correction for attr

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -47,7 +47,7 @@ def before_request() -> None:
     g.user = user
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """
     babel locale selector decorator
@@ -57,9 +57,7 @@ def get_locale():
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
